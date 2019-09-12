75d18141c86e312f4dadc0d96afcebb30b3fab75
yumiova <yumiova@protonmail.ch>
2019-09-12 19:22:16 +0200

    Register all event type cases

--- libevdev/System/Evdev/Event.hs
+++ libevdev/System/Evdev/Event.hs
@@ -1,1 +1,33 @@
-module System.Evdev.Event () where
+module System.Evdev.Event
+  ( Ev
+      ( SynEv,
+        KeyEv,
+        RelEv,
+        AbsEv,
+        MscEv,
+        SwEv,
+        LedEv,
+        SndEv,
+        RepEv,
+        FfEv,
+        PwrEv,
+        FfStatusEv,
+        MaxEv
+        )
+    )
+where
+
+data Ev
+  = SynEv
+  | KeyEv
+  | RelEv
+  | AbsEv
+  | MscEv
+  | SwEv
+  | LedEv
+  | SndEv
+  | RepEv
+  | FfEv
+  | PwrEv
+  | FfStatusEv
+  | MaxEv
