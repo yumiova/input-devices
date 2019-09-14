321e0be5e400b2a592bc75c650fd8c66a4b98816
yumiova <yumiova@protonmail.ch>
2019-09-14 18:07:13 +0200

    Bind `struct input_event` in terms of `InputEvent`

--- libevdev/System/Evdev.hs
+++ libevdev/System/Evdev.hs
@@ -1,1 +1,22 @@
-module System.Evdev () where
+module System.Evdev
+  ( InputEvent
+      ( InputEvent,
+        inputEventTime,
+        inputEventType,
+        inputEventCode,
+        inputEventValue
+        )
+    )
+where
+
+import Data.Int (Int32)
+import Data.Word (Word16)
+import System.Evdev.Time (Timeval)
+
+data InputEvent
+  = InputEvent
+      { inputEventTime :: Timeval,
+        inputEventType :: Word16,
+        inputEventCode :: Word16,
+        inputEventValue :: Int32
+        }
