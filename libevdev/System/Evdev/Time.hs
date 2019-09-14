0a2ad4c22650dc792a0c98259f86784adc6bd343
yumiova <yumiova@protonmail.ch>
2019-09-14 17:39:20 +0200

    Bind `struct timeval` in terms of `Timeval`

--- libevdev/System/Evdev/Time.hs
+++ libevdev/System/Evdev/Time.hs
@@ -1,1 +1,8 @@
-module System.Evdev.Time () where
+module System.Evdev.Time
+  ( Timeval (Timeval, timevalSec, timevalUsec)
+    )
+where
+
+import Foreign.C (CSUSeconds, CTime)
+
+data Timeval = Timeval {timevalSec :: CTime, timevalUsec :: CSUSeconds}
