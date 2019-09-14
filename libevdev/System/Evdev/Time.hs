41283ec417016b98d7a14096d635022b39158ec3
yumiova <yumiova@protonmail.ch>
2019-09-14 17:55:19 +0200

    Export context associated with time module

--- libevdev/System/Evdev/Time.hs
+++ libevdev/System/Evdev/Time.hs
@@ -3,13 +3,15 @@
 {-# LANGUAGE ViewPatterns #-}
 
 module System.Evdev.Time
-  ( Timeval (Timeval, timevalSec, timevalUsec)
+  ( Timeval (Timeval, timevalSec, timevalUsec),
+    timevalCtx
     )
 where
 
 import Foreign (Storable (alignment, peek, poke, sizeOf), castPtr)
 import Foreign.C (CInt (CInt), CSUSeconds (CSUSeconds), CTime (CTime))
 import qualified Language.C.Inline as C (block, exp, include, pure)
+import Language.C.Inline.Context (Context)
 
 C.include "<sys/time.h>"
 
@@ -36,3 +38,6 @@ instance Storable Timeval where
       ((struct timeval *) $(void *target))->tv_sec = $(time_t sec);
       ((struct timeval *) $(void *target))->tv_usec = $(suseconds_t usec);
     } |]
+
+timevalCtx :: Context
+timevalCtx = mempty
