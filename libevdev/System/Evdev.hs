0d55551a1006466fc4d74ff6b9ac8906a7288767
yumiova <yumiova@protonmail.ch>
2019-09-16 16:37:26 +0200

    Reexport all bound data types

--- libevdev/System/Evdev.hs
+++ libevdev/System/Evdev.hs
@@ -1,1 +1,30 @@
-module System.Evdev () where
+module System.Evdev
+  ( Timeval (Timeval, timevalSec, timevalUsec),
+    InputEvent (InputEvent, inputEventCode, inputEventTime, inputEventType, inputEventValue),
+    InputAbsinfo
+      ( InputAbsinfo,
+        inputAbsinfoFlat,
+        inputAbsinfoFuzz,
+        inputAbsinfoMaximum,
+        inputAbsinfoMinimum,
+        inputAbsinfoResolution,
+        inputAbsinfoValue
+        )
+    )
+where
+
+import System.Evdev.Input
+  ( InputAbsinfo
+      ( InputAbsinfo,
+        inputAbsinfoFlat,
+        inputAbsinfoFuzz,
+        inputAbsinfoMaximum,
+        inputAbsinfoMinimum,
+        inputAbsinfoResolution,
+        inputAbsinfoValue
+        ),
+    InputEvent (InputEvent, inputEventCode, inputEventTime, inputEventType, inputEventValue)
+    )
+import System.Evdev.Time
+  ( Timeval (Timeval, timevalSec, timevalUsec)
+    )
