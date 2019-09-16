969c3466433ab2387514e9d2c4c0bbc02d0fd263
yumiova <yumiova@protonmail.ch>
2019-09-16 20:01:44 +0200

    Register data type for representing input device control matchers

--- library/Input/Control.hs
+++ library/Input/Control.hs
@@ -1,1 +1,10 @@
-module Input.Control () where
+module Input.Control
+  ( Control ((:<))
+    )
+where
+
+import System.Libevdev (InputEvent)
+
+infixr 5 :<
+
+data Control a = a :< (InputEvent -> Control a)
