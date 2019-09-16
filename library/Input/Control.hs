ad1f95d75838261f280050ea11247a09b39b95c6
yumiova <yumiova@protonmail.ch>
2019-09-16 20:04:28 +0200

    Implement functorial instances for input device controls

--- library/Input/Control.hs
+++ library/Input/Control.hs
@@ -3,8 +3,18 @@ module Input.Control
     )
 where
 
+import Control.Applicative (liftA2)
 import System.Libevdev (InputEvent)
 
 infixr 5 :<
 
 data Control a = a :< (InputEvent -> Control a)
+
+instance Functor Control where
+  fmap f ~(a :< as) = f a :< (fmap f <$> as)
+
+instance Applicative Control where
+
+  pure a = a :< const (pure a)
+
+  ~(f :< fs) <*> ~(a :< as) = f a :< liftA2 (<*>) fs as
