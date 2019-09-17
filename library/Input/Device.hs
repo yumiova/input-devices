26adb5a1de82c64bbcf5fd6b93d3af5102b5e43a
yumiova <yumiova@protonmail.ch>
2019-09-17 23:01:57 +0200

    Open associated file descriptors for each input device

--- library/Input/Device.hs
+++ library/Input/Device.hs
@@ -1,11 +1,16 @@
 module Input.Device (observe) where
 
 import Control.Concurrent.Async (forConcurrently_)
+import Control.Exception (bracket)
 import Input.Control (Control)
 import System.Directory (listDirectory)
+import System.Posix.IO (OpenMode (ReadOnly), closeFd, defaultFileFlags, openFd)
 
 observePath :: FilePath -> Control a -> IO ()
-observePath filePath _ = print filePath
+observePath filePath _ = withFd $ \fd -> print (filePath, fd)
+  where
+    flags = defaultFileFlags
+    withFd = bracket (openFd filePath ReadOnly Nothing flags) closeFd
 
 observe :: Control a -> IO ()
 observe control = do
