077c7bee0a5624c2311301558e63f760528424d6
yumiova <yumiova@protonmail.ch>
2019-09-17 22:37:27 +0200

    Handle individual event devices separately

--- library/Input/Device.hs
+++ library/Input/Device.hs
@@ -4,8 +4,13 @@ import Control.Concurrent.Async (forConcurrently_)
 import Input.Control (Control)
 import System.Directory (listDirectory)
 
+observePath :: FilePath -> Control a -> IO ()
+observePath filePath _ = print filePath
+
 observe :: Control a -> IO ()
-observe _ = do
-  inputDevices <- listDirectory "/dev/input"
+observe control = do
+  inputDevices <- listDirectory prefix
   let eventDevices = filter ((== "event") . take 5) inputDevices
-  forConcurrently_ eventDevices print
+  forConcurrently_ eventDevices ((`observePath` control) . (prefix <>))
+  where
+    prefix = "/dev/input/"
