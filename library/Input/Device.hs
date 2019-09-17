89845d44674652410cc68c47d96f769724e5219d
yumiova <yumiova@protonmail.ch>
2019-09-18 01:14:11 +0200

    Extract every single event for a given frame

--- library/Input/Device.hs
+++ library/Input/Device.hs
@@ -9,12 +9,13 @@ where
 import Control.Concurrent (threadDelay)
 import Control.Concurrent.Async (forConcurrently_)
 import Control.Exception (bracket)
+import Data.Foldable (foldl')
 import Foreign (Ptr, alloca, peek)
 import Foreign.C (CInt (CInt))
 import Input.Control (Control ((:<)))
-import qualified Language.C.Inline as C (baseCtx, context, exp, include)
+import qualified Language.C.Inline as C (baseCtx, context, exp, include, pure)
 import System.Directory (listDirectory)
-import System.Libevdev (Libevdev, libevdevCtx)
+import System.Libevdev (InputEvent, Libevdev, libevdevCtx)
 import System.Posix.IO
   ( OpenFileFlags (nonBlock),
     OpenMode (ReadOnly),
@@ -26,22 +27,45 @@ import System.Posix.Types (Fd (Fd))
 
 C.context (C.baseCtx <> libevdevCtx)
 
+C.include "<errno.h>"
+
 C.include "<libevdev/libevdev.h>"
 
+popEvents :: Ptr Libevdev -> IO [InputEvent]
+popEvents libevdev = alloca go
+  where
+    dispatch status target
+      | status == [C.pure| int { -EAGAIN } |] = pure []
+      | status == [C.pure| int { LIBEVDEV_READ_STATUS_SUCCESS } |] =
+        do
+          event <- peek target
+          (event :) <$> go target
+      | status == [C.pure| int { LIBEVDEV_READ_STATUS_SYNC } |] =
+        do
+          putStrLn "unimplemented: input device syncing"
+          pure []
+      | otherwise = error ("unimplemented: status" <> show status)
+    go target = do
+      status <-
+        [C.exp| int {
+          libevdev_next_event(
+            $(struct libevdev *libevdev),
+            LIBEVDEV_READ_FLAG_NORMAL,
+            $(struct input_event *target)
+          )
+        } |]
+      dispatch status target
+
+observeStep :: Ptr Libevdev -> Control a -> IO (Control a)
+observeStep libevdev control = do
+  events <- popEvents libevdev
+  pure (foldl' (\(_ :< f) event -> f event) control events)
+
 observeDevice :: Show a => Ptr Libevdev -> Control a -> IO ()
-observeDevice libevdev ~(a :< f) =
-  alloca $ \eventPtr -> do
-    print a
-    [C.exp| void {
-      libevdev_next_event(
-        $(struct libevdev *libevdev),
-        LIBEVDEV_READ_FLAG_NORMAL,
-        $(struct input_event *eventPtr)
-      )
-    } |]
-    threadDelay 8192
-    event <- peek eventPtr
-    observeDevice libevdev (f event)
+observeDevice libevdev control = do
+  increment <- observeStep libevdev control
+  threadDelay 8192
+  observeDevice libevdev increment
 
 observePath :: Show a => FilePath -> Control a -> IO ()
 observePath filePath control =
