c71fee4f6c09dc02b1859ca4ba06acdce68fb7ad
yumiova <yumiova@protonmail.ch>
2019-09-17 22:27:19 +0200

    Print all relevant event devices

--- library/Input/Device.hs
+++ library/Input/Device.hs
@@ -1,6 +1,10 @@
 module Input.Device (observe) where
 
 import Input.Control (Control)
+import System.Directory (listDirectory)
 
 observe :: Control a -> IO ()
-observe _ = pure ()
+observe _ = do
+  inputDevices <- listDirectory "/dev/input"
+  let eventDevices = filter ((== "event") . take 5) inputDevices
+  print eventDevices
