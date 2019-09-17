f4c8fd926b6c32c8a22c7aca21e195c434707d8a
yumiova <yumiova@protonmail.ch>
2019-09-17 23:10:01 +0200

    Open `libevdev` devices for each event stream

--- library/Input/Device.hs
+++ library/Input/Device.hs
@@ -1,9 +1,18 @@
-module Input.Device (observe) where
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Input.Device
+  ( observe
+    )
+where
 
 import Control.Concurrent.Async (forConcurrently_)
 import Control.Exception (bracket)
+import Foreign.C (CInt (CInt))
 import Input.Control (Control)
+import qualified Language.C.Inline as C (baseCtx, context, exp, include)
 import System.Directory (listDirectory)
+import System.Libevdev (libevdevCtx)
 import System.Posix.IO
   ( OpenFileFlags (nonBlock),
     OpenMode (ReadOnly),
@@ -11,12 +20,24 @@ import System.Posix.IO
     defaultFileFlags,
     openFd
     )
+import System.Posix.Types (Fd (Fd))
+
+C.context (C.baseCtx <> libevdevCtx)
+
+C.include "<libevdev/libevdev.h>"
 
 observePath :: FilePath -> Control a -> IO ()
-observePath filePath _ = withFd $ \fd -> print (filePath, fd)
+observePath filePath _ =
+  withFd $ \(Fd fd) -> withLibevdev $ \libevdev -> do
+    [C.exp| void { libevdev_set_fd($(struct libevdev *libevdev), $(int fd)) } |]
+    print (filePath, fd, libevdev)
   where
     flags = defaultFileFlags {nonBlock = True}
     withFd = bracket (openFd filePath ReadOnly Nothing flags) closeFd
+    withLibevdev =
+      bracket
+        [C.exp| struct libevdev * { libevdev_new() } |]
+        (\libevdev -> [C.exp| void { libevdev_free($(struct libevdev *libevdev)) } |])
 
 observe :: Control a -> IO ()
 observe control = do
