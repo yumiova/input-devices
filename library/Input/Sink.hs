6c682568388ecaaf814e5a4aaa8c42997f8ffe93
yumiova <yumiova@protonmail.ch>
2019-09-20 16:44:33 +0200

    Implement `Contravariant` instance for sink streams

--- library/Input/Sink.hs
+++ library/Input/Sink.hs
@@ -4,9 +4,13 @@ module Input.Sink
     )
 where
 
+import Data.Functor.Contravariant (Contravariant (contramap))
 import System.Libevdev (InputEvent)
 
 -- * Internal streams
 infixr 5 :<
 
 data Stream a = [InputEvent] :< (a -> Stream a)
+
+instance Contravariant Stream where
+  contramap f ~(bevent :< bs) = bevent :< contramap f . bs . f
