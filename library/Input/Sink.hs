c161031054f715b8eb15a750f9a873de6d047121
yumiova <yumiova@protonmail.ch>
2019-09-20 17:35:15 +0200

    Implement `Divisible` instance for sink streams

--- library/Input/Sink.hs
+++ library/Input/Sink.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Input.Sink
   ( -- * Internal streams
     Stream ((:<))
@@ -5,6 +7,7 @@ module Input.Sink
 where
 
 import Data.Functor.Contravariant (Contravariant (contramap))
+import Data.Functor.Contravariant.Divisible (Divisible (conquer, divide))
 import System.Libevdev (InputEvent)
 
 -- * Internal streams
@@ -14,3 +17,10 @@ data Stream a = [InputEvent] :< (a -> Stream a)
 
 instance Contravariant Stream where
   contramap f ~(bevent :< bs) = bevent :< contramap f . bs . f
+
+instance Divisible Stream where
+
+  divide f ~(bevent :< bs) ~(cevent :< cs) =
+    bevent <> cevent :< \(f -> ~(b, c)) -> divide f (bs b) (cs c)
+
+  conquer = [] :< const conquer
